3ff4a90823487c912648eb0390685814c44cefc8
RickyLin <rickykys92424@gmail.com>
2022-06-09 16:05:46 +0800

    出入庫紀錄查詢

---
+++ Stroage.API/Controllers/ActionLogController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stroage.API.RequestModels;
+
+namespace Stroage.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActionLogController : ControllerBase
+    {
+        private readonly StorageContext _context;
+
+        public ActionLogController(StorageContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<DeserializedActionLog>> Get(DateTime? beginTime = null, DateTime? endTime = null)
+        {
+            if (!beginTime.HasValue)
+                beginTime = DateTime.Now.AddDays(-7);
+            if (!endTime.HasValue)
+                endTime = DateTime.Now;            
+            var logs = await _context.ActionLogs
+                .Include(l => l.Person)
+                .Include(l => l.Pack)
+                .ThenInclude(p => p.Material)
+                .Include(l => l.Bin)
+                .AsNoTracking()
+                .Where(l => l.CreateTime >= beginTime && l.CreateTime <= endTime)
+                .Select(l => new DeserializedActionLog
+                {
+                    UserName = l.Person.Name,
+                    BinName = l.Bin.Name,
+                    MaterialDesc = l.Pack.Material.Description,
+                    Quantity = l.Pack.Quantity,
+                    Operation = l.IsIn ? "入庫" : "出庫",
+                    OpTime = l.CreateTime,
+                })
+                .ToArrayAsync();
+            return logs;
+        }
+    }
+}
---
+++ Stroage.API/RequestModels/DeserializedActionLog.cs
@@ -0,0 +1,12 @@
+﻿namespace Stroage.API.RequestModels
+{
+    public class DeserializedActionLog
+    {
+        public string UserName { get; set; }
+        public string BinName { get; set; }
+        public string MaterialDesc { get; set; }
+        public int Quantity { get; set; }
+        public string Operation { get; set; }
+        public DateTime OpTime { get; set; }
+    }
+}
